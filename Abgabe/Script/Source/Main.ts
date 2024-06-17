d0648e8ed28f71a0ca6646d0d8a209aba5fe35d5
Niclas-Cravaack <60705949+Niclas-Cravaack@users.noreply.github.com>
2024-06-17 19:54:50 +0200

    external data hinzugefügt

--- Abgabe/Script/Source/Main.ts
+++ Abgabe/Script/Source/Main.ts
@@ -1,13 +1,20 @@
 namespace Script {
+  interface Data{
+    cameraDistance: number
+  }
+
   import f = FudgeCore;
   f.Debug.info("Main Program Template running!");
   
   let viewport: f.Viewport;
-  
+  let camera: f.ComponentCamera;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
+  export let data: Data;
+
   function start(_event: CustomEvent): void {
-    
+    camera = new f.ComponentCamera();
+    camera.mtxPivot.translate(new f.Vector3(-0.5,1,data.cameraDistance))
     viewport = _event.detail;
 
 
